e1e625804385a3c37ca7ea6f8ee170ea87e412a0
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2024-01-27 17:35:08 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    tracing_subscriber::fmt::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let _ = event_loop.run(move |event, elwt| match event {
+        Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                elwt.exit();
+            }
+            WindowEvent::KeyboardInput {
+                device_id,
+                event,
+                is_synthetic,
+            } => {
+                println!("The escape key was pressed; stopping");
+                elwt.exit();
+            }
+            _ => {}
+        },
+        _ => {}
+    });
+}
